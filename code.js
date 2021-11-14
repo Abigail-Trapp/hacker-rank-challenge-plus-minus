df665b120e50e07e1af1feafb6a8d8a4e0676ffb
Abigail Trapp <abbsthepandahead@gmail.com>
2021-11-13 21:26:19 -0700

    initial css commit

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+// let enter = document.getElementById("enter")
+// enter.addEventListener("click",plusMinus)
+
 function plusMinus(arr) {
     // Write your code here
      let INTEGER_ARRAY = [...arr]
@@ -11,6 +14,24 @@ function plusMinus(arr) {
      console.log(calcPos/arrayLength)
      console.log(calcNeg/arrayLength)
      console.log(calcZero/arrayLength)
-     
+
+    //  let positives=document.querySelector("positives")
+    //  let posNumber = document.createElement("div")
+    //  posNumber.innerHTML=`${calcPos}`
+    //  positives.append(posNumber)
+
+    //  let negatives=document.querySelector("negatives")
+    //  let negNumber=document.createElement("div")
+    //  negNumber.innerHTML=`${calcNeg}`
+    //  negatives.append(negNumber)
+
+    //  let zeros=document.querySelector("zeros")
+    //  let zeroNumber=document.createElement("div")
+    //  zeroNumber.innerHTML=`${calcZero}`
+    //  zeros.append(zeroNumber)
+
+
 }
-console.log(plusMinus([-2,-1,0,1,2]))
+plusMinus([-1,1,0,1,2])
+// let arrayValue = document.querySelector("arrayvalue").value; 
+// plusMinus(arrayValue)
